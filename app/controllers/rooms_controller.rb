1a3d2abf4c68acc62bee3aea38b5a108a0e9d19a
EC2 Default User <ec2-user@ip-172-31-1-71.ap-northeast-1.compute.internal>
2021-06-07 02:41:29 +0000

    first commit

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,32 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+  
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+      render :new
+    end
+  end
+
+  def edit
+  end
+  
+  private
+    def room_params
+      params.require(:room).permit(:name, :title,:created_at)
+    end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'rooms#index'
+  resources :rooms
+end
