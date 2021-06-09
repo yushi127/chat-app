026eed675ebab0671de77de7a39c704b6e646302
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-06-09 18:36:16 +0900

    消去機能追加、見た目変更しました

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -29,6 +29,12 @@ class RoomsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to root_path, notice: 'Success!'
+  end
   
   private
     def room_params
