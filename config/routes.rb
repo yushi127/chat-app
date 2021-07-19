d5f80b17b0a5178edf561d07263274dc8aef6771
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-07-19 12:51:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,25 @@ Rails.application.routes.draw do
   root 'rooms#index'
   # root "home#index"
   get 'search' => 'rooms#search'
+  get 'setting' => 'rooms#setting'
+
   resources :rooms
   resources :users
   get 'chatroom' => 'chatroom#show'
   get 'chatroom' => 'chatroom#create'
   mount ActionCable.server => '/cable'
 end
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root 'rooms#index'
+#   get 'search' => 'rooms#search'
+#   get 'setting' => 'rooms#setting'
+
+#   # get 'myroom' => 'myrooms#index'
+#   # delete 'myrooms/:id' => 'myrooms#destroy'
+#   # delete 'relationdes' => 'rooms#relationdes'
+#   # delete 'room/:id', to: 'rooms#relationdes'
+#   resources :myrooms
+#   resources :rooms
+# end
