f0000caa8bd9442ee0233b4ae2bcd28b231d8373
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-07-19 17:01:14 +0900

    アイコンのバグ修正しました

--- app/assets/javascripts/icon_preview.js
+++ app/assets/javascripts/icon_preview.js
@@ -1,26 +1,3 @@
-// $(function(){
-//   $fileField = $('#file-input')
-
-//   $($fileField).on('change', $fileField, function(e) {
-//     file = e.target.files[0]
-//     reader = new FileReader(),
-//     $preview = $('#img_field');
-
-//     reader.onload = (function(file) {
-//       return function(e) {
-//         $preview.empty();
-//         $preview.append($('<img>').attr({
-//           src: e.target.result,
-//           width: "100%",
-//           class: "preview",
-//           title: file.name
-//         }));
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
-
 $( document ).on('turbolinks:load', function() {
   function readURL(input) {
     if (input.files && input.files[0]) {
