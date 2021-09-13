e9e69b6315b5563ef2f11312e7e55861c91d8610
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-09-14 02:25:52 +0900

    Update myrooms_controller.rb

--- app/controllers/myrooms_controller.rb
+++ app/controllers/myrooms_controller.rb
@@ -25,32 +25,26 @@ class MyroomsController < ApplicationController
     @rooms = Room.all  
     @rerations =  Relation.all  
     @search_params = room_search_params  #検索結果の画面で、フォームに検索した値を表示するために、paramsの値をビューで使えるようにする
+
+    # すべてのROOMから検索した結果を入れる
     @searchrooms = Room.search(@search_params)  #Reservationモデルのsearchを呼び出し、引数としてparamsを渡している。
-    # ///////////////////////////////////////////////////////
-#     @relationusers = Relation.where(userid: @user.id)
-#     @aa = @relationusers
-#     count = @searchrooms.length
-#     i = 0
-#     @relationrooms = []
-#     while i < count 
-#       @aaa = @searchrooms.where(id: @relationusers[i].roomid).first
-# # @relationmyroom
-#       @relationroom = Room.where(id: @relationmyroom.id)
-#       @relationrooms.push(@relationroom)
-#       i+=1
-#     end
-    @relationrooms = []
-    @relationusers = Relation.where(userid: @user.id)
-    @aa = @relationusers
+    
+    # rerationから現在ログイン中のユーザーのレコード取得
+    relationusers = Relation.where(userid: @user.id)
+
+    # 変数定義
     i = 0
-    counti = @relationusers.length
+    @relationrooms = []
+    counti = relationusers.length
     countj = @searchrooms.length
 
+    # ルームのIDがrerationから現在ログイン中のユーザーのレコードとすべてのROOMから検索した結果が同じとこ取得
+    # 取得したものを@relationroomsのPUSH
     while i <  counti 
       # byebug
       j=0
         while j <  countj
-          @relationmyroom = Room.where(id: @searchrooms[j].id).where(id:@relationusers[i].roomid).first
+          @relationmyroom = Room.where(id: @searchrooms[j].id).where(id:relationusers[i].roomid).first
           unless @relationmyroom.blank?  
             @relationrooms.push(@relationmyroom)
           end
